303ee267033d5570feb8aef4af5f4622a47ea9cb
Tracy <ymailingtracy@yahoo.com>
2020-09-26 20:18:37 -0700

    Trying to get reviewed

--- server/router.js
+++ server/router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const controllers = require('./controllers.js');
 
 router
-  .route('/camp')
-  .get((req, res) => {
+  .route('/:id')
+  .get(controllers.getOne)
 
-  })
+router
+  .route('/all')
+  .get(controllers.getAll)
 
 module.exports = router;
